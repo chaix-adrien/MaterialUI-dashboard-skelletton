03d9fb79c7ca7143e1852cb36749a8cf30296083
chaixa <adrien.chaix@protonmail.com>
2020-03-13 13:49:22 +0100

    turnSetState in promise

--- src/index.js
+++ src/index.js
@@ -15,19 +15,24 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import "assets/css/material-dashboard-react.css?v=1.8.0";
+import "assets/css/material-dashboard-react.css?v=1.8.0"
 import { siteName } from "variables/config.json"
-import logo from "assets/img/Logo.png";
-import Notifier from "components/Snackbar/Notifier";
-import Confirm from "components/Confirm";
-import { createBrowserHistory } from "history";
-import React from "react";
-import ReactDOM from "react-dom";
-import Provider from 'react-redux/es/components/Provider';
-import { Router as DomRouter } from "react-router-dom";
-import store from './redux';
-import Router from "./Router.js";
-const hist = createBrowserHistory();
+import logo from "assets/img/Logo.png"
+import Notifier from "components/Snackbar/Notifier"
+import Confirm from "components/Confirm"
+import { createBrowserHistory } from "history"
+import React from "react"
+import ReactDOM from "react-dom"
+import Provider from "react-redux/es/components/Provider"
+import { Router as DomRouter } from "react-router-dom"
+import store from "./redux"
+import Router from "./Router.js"
+const hist = createBrowserHistory()
+
+const setState = React.Component.prototype.setState
+React.Component.prototype.setState = function(newState) {
+  return new Promise(resolve => setState.call(this, newState, resolve))
+}
 
 window.siteLogo = logo
 window.document.title = siteName
@@ -35,10 +40,9 @@ ReactDOM.render(
   <Provider store={store}>
     <Notifier />
     <Confirm />
-    <DomRouter history={hist} >
+    <DomRouter history={hist}>
       <Router />
     </DomRouter>
-  </Provider>
-  ,
+  </Provider>,
   document.getElementById("root")
-);
+)
