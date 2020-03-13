176c334f2a4417f2d64c1aa8926ad7c75eea973a
chaixa <adrien.chaix@protonmail.com>
2020-03-13 13:49:10 +0100

    can use string icon for notif

--- src/components/Snackbar/Snackbar.js
+++ src/components/Snackbar/Snackbar.js
@@ -1,54 +1,49 @@
-import React from "react";
-import classNames from "classnames";
-import PropTypes from "prop-types";
+import React from "react"
+import classNames from "classnames"
+import PropTypes from "prop-types"
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-import Snack from "@material-ui/core/Snackbar";
-import IconButton from "@material-ui/core/IconButton";
+import { makeStyles } from "@material-ui/core/styles"
+import Snack from "@material-ui/core/Snackbar"
+import IconButton from "@material-ui/core/IconButton"
 // @material-ui/icons
-import Close from "@material-ui/icons/Close";
+import Close from "@material-ui/icons/Close"
 // core components
-import styles from "assets/jss/material-dashboard-react/components/snackbarContentStyle.js";
+import styles from "assets/jss/material-dashboard-react/components/snackbarContentStyle.js"
 
-const useStyles = makeStyles(styles);
+const useStyles = makeStyles(styles)
 
 export default function Snackbar(props) {
-  const classes = useStyles();
-  const { message, color, close, icon, place, open, rtlActive } = props;
-  var action = [];
+  const classes = useStyles()
+  const { message, color, close, icon, place, open, rtlActive } = props
+  var action = []
   const messageClasses = classNames({
-    [classes.iconMessage]: icon !== undefined
-  });
+    [classes.iconMessage]: icon !== undefined,
+  })
   if (close !== undefined) {
     action = [
-      <IconButton
-        className={classes.iconButton}
-        key="close"
-        aria-label="Close"
-        color="inherit"
-        onClick={() => props.closeNotification()}
-      >
+      <IconButton className={classes.iconButton} key='close' aria-label='Close' color='inherit' onClick={() => props.closeNotification()}>
         <Close className={classes.close} />
-      </IconButton>
-    ];
+      </IconButton>,
+    ]
   }
   return (
     <Snack
       autoHideDuration={2000}
       anchorOrigin={{
         vertical: place.indexOf("t") === -1 ? "bottom" : "top",
-        horizontal:
-          place.indexOf("l") !== -1
-            ? "left"
-            : place.indexOf("c") !== -1
-              ? "center"
-              : "right"
+        horizontal: place.indexOf("l") !== -1 ? "left" : place.indexOf("c") !== -1 ? "center" : "right",
       }}
       open={open}
       onClose={props.closeNotification}
       message={
         <div>
-          {icon !== undefined ? <props.icon className={classes.icon} /> : null}
+          {icon !== undefined ? (
+            typeof icon === "string" ? (
+              <Icon className={classes.icon}>{icon}</Icon>
+            ) : (
+              <props.icon className={classes.icon} />
+            )
+          ) : null}
           <span className={messageClasses}>{message}</span>
         </div>
       }
@@ -57,11 +52,11 @@ export default function Snackbar(props) {
         classes: {
           root: classes.root + " " + classes[color],
           message: classes.message,
-          action: classNames({ [classes.actionRTL]: rtlActive })
-        }
+          action: classNames({ [classes.actionRTL]: rtlActive }),
+        },
       }}
     />
-  );
+  )
 }
 
 Snackbar.propTypes = {
@@ -72,5 +67,5 @@ Snackbar.propTypes = {
   place: PropTypes.oneOf(["tl", "tr", "tc", "br", "bl", "bc"]),
   open: PropTypes.bool,
   rtlActive: PropTypes.bool,
-  closeNotification: PropTypes.func
-};
+  closeNotification: PropTypes.func,
+}
