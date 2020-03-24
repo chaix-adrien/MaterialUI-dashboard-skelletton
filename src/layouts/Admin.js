bf430dda0f506ad9eb4f21505391a59a88450b9a
chaixa <adrien.chaix@protonmail.com>
2020-03-24 16:06:30 +0100

    increase default wheelspeed

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -1,23 +1,20 @@
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-import bgImage from "assets/img/Sidebar.jpg";
-import styles from "assets/jss/material-dashboard-react/layouts/adminStyle.js";
+import { makeStyles } from "@material-ui/core/styles"
+import bgImage from "assets/img/Sidebar.jpg"
+import styles from "assets/jss/material-dashboard-react/layouts/adminStyle.js"
 // core components
-import Navbar from "components/Navbars/Navbar.js";
-import Sidebar from "components/Sidebar/Sidebar.js";
+import Navbar from "components/Navbars/Navbar.js"
+import Sidebar from "components/Sidebar/Sidebar.js"
 // creates a beautiful scrollbar
-import PerfectScrollbar from "perfect-scrollbar";
-import "perfect-scrollbar/css/perfect-scrollbar.css";
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import routes from "routes.js";
-import View404 from "views/View404.js";
+import PerfectScrollbar from "perfect-scrollbar"
+import "perfect-scrollbar/css/perfect-scrollbar.css"
+import React from "react"
+import { Route, Switch } from "react-router-dom"
+import routes from "routes.js"
+import View404 from "views/View404.js"
 import { siteName } from "variables/config.json"
 
-
-
-
-let ps;
+let ps
 
 const switchRoutes = (_routes, classes) => {
   let out = _routes.map((prop, key) => {
@@ -26,14 +23,15 @@ const switchRoutes = (_routes, classes) => {
         <Route
           exact
           path={(prop.layout || "") + prop.path + (prop.params || "")}
-          render={(props) => {
-            if (prop.hideHeader)
-              return <prop.component {...props} />
+          render={props => {
+            if (prop.hideHeader) return <prop.component {...props} />
             else
-              return <>
-                <div className={classes.headerFill} />
-                <prop.component {...props} />
-              </>
+              return (
+                <>
+                  <div className={classes.headerFill} />
+                  <prop.component {...props} />
+                </>
+              )
           }}
           key={key}
         />
@@ -48,37 +46,38 @@ const switchRoutes = (_routes, classes) => {
   return out
 }
 
-const useStyles = makeStyles(styles);
+const useStyles = makeStyles(styles)
 
 export default function Admin({ ...rest }) {
-  const classes = useStyles();
-  const mainPanel = React.createRef();
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const classes = useStyles()
+  const mainPanel = React.createRef()
+  const [mobileOpen, setMobileOpen] = React.useState(false)
   const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
+    setMobileOpen(!mobileOpen)
+  }
   const resizeFunction = () => {
     if (window.innerWidth >= 960) {
-      setMobileOpen(false);
+      setMobileOpen(false)
     }
-  };
+  }
 
   React.useEffect(() => {
     if (navigator.platform.indexOf("Win") > -1) {
       ps = new PerfectScrollbar(mainPanel.current, {
         suppressScrollX: true,
-        suppressScrollY: false
-      });
-      document.body.style.overflow = "hidden";
+        suppressScrollY: false,
+        wheelSpeed: 7,
+      })
+      document.body.style.overflow = "hidden"
     }
-    window.addEventListener("resize", resizeFunction);
+    window.addEventListener("resize", resizeFunction)
     return function cleanup() {
       if (navigator.platform.indexOf("Win") > -1) {
-        ps.destroy();
+        ps.destroy()
       }
-      window.removeEventListener("resize", resizeFunction);
-    };
-  }, [mainPanel]);
+      window.removeEventListener("resize", resizeFunction)
+    }
+  }, [mainPanel])
   return (
     <div className={classes.wrapper}>
       <Sidebar
@@ -92,13 +91,8 @@ export default function Admin({ ...rest }) {
         {...rest}
       />
       <div className={classes.mainPanel} ref={mainPanel}>
-        <Navbar
-          routes={routes}
-          handleDrawerToggle={handleDrawerToggle}
-          {...rest}
-        />
+        <Navbar routes={routes} handleDrawerToggle={handleDrawerToggle} {...rest} />
         <div className={classes.content}>
-
           <div className={classes.container}>
             <Switch>
               {switchRoutes(routes, classes)}
@@ -108,5 +102,5 @@ export default function Admin({ ...rest }) {
         </div>
       </div>
     </div>
-  );
+  )
 }
