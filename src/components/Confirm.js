0854a85d040d97de4b6f9470dacc05aa597fc8ce
chaixa <adrien.chaix@protonmail.com>
2020-03-25 14:56:41 +0100

    better css for confirm withjout desc

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import PubSub from 'pubsub-js'
+import React from "react"
+import Button from "@material-ui/core/Button"
+import Dialog from "@material-ui/core/Dialog"
+import DialogActions from "@material-ui/core/DialogActions"
+import DialogContent from "@material-ui/core/DialogContent"
+import DialogContentText from "@material-ui/core/DialogContentText"
+import DialogTitle from "@material-ui/core/DialogTitle"
+import PubSub from "pubsub-js"
 
-PubSub.confirm = (confirm) => PubSub.publish('CONFIRM', confirm)
+PubSub.confirm = confirm => PubSub.publish("CONFIRM", confirm)
 
 export default class Confirm extends React.PureComponent {
   constructor(props) {
@@ -16,17 +16,16 @@ export default class Confirm extends React.PureComponent {
   }
 
   componentDidMount() {
-    this.sub = PubSub.subscribe('CONFIRM', (_, confirm) => this.setState({ confirm }));
+    this.sub = PubSub.subscribe("CONFIRM", (_, confirm) => this.setState({ confirm }))
   }
 
   componentWillUnmount() {
     PubSub.unsubscribe(this.sub)
   }
 
-  onBtnClicked = (cb) => {
+  onBtnClicked = cb => {
     this.setState({ confirm: null })
-    if (cb)
-      cb()
+    if (cb) cb()
   }
 
   render() {
@@ -37,29 +36,35 @@ export default class Confirm extends React.PureComponent {
         <Dialog
           open={confirm ? true : false}
           onClose={_ => this.setState({ confirm: null })}
-          aria-labelledby="alert-dialog-title"
-          PaperProps={{ style: { minWidth: 400, minHeight: 200 } }}
-          aria-describedby="alert-dialog-description"
-        >
-          {confirm.title && <DialogTitle id="alert-dialog-title">{confirm.title}</DialogTitle>}
-          {confirm.subtitle && <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              {confirm.subtitle}
-            </DialogContentText>
-          </DialogContent>}
+          aria-labelledby='alert-dialog-title'
+          PaperProps={{ style: { minWidth: 400, minHeight: confirm.subtitle ? 200 : 100, justifyContent: "space-between" } }}
+          aria-describedby='alert-dialog-description'>
+          {confirm.title && <DialogTitle id='alert-dialog-title'>{confirm.title}</DialogTitle>}
+          {confirm.subtitle && (
+            <DialogContent>
+              <DialogContentText id='alert-dialog-description'>{confirm.subtitle}</DialogContentText>
+            </DialogContent>
+          )}
           <DialogActions>
-            {!confirm.buttons && <><Button onClick={_ => this.onBtnClicked(confirm.onDisagree)} color="primary">
-              Annuler
-            </Button>
-              <Button onClick={_ => this.onBtnClicked(confirm.onAgree)} color="primary" autoFocus>
-                Valider
-          </Button></>}
-            {confirm.buttons && confirm.buttons.map((btn, id) => <Button key={id} onClick={_ => this.onBtnClicked(btn.onClick)} color="primary">
-              {btn.txt}
-            </Button>)}
+            {!confirm.buttons && (
+              <>
+                <Button onClick={_ => this.onBtnClicked(confirm.onDisagree)} color='primary'>
+                  Annuler
+                </Button>
+                <Button onClick={_ => this.onBtnClicked(confirm.onAgree)} color='primary' autoFocus>
+                  Valider
+                </Button>
+              </>
+            )}
+            {confirm.buttons &&
+              confirm.buttons.map((btn, id) => (
+                <Button key={id} onClick={_ => this.onBtnClicked(btn.onClick)} color='primary'>
+                  {btn.txt}
+                </Button>
+              ))}
           </DialogActions>
         </Dialog>
-      </div >
-    );
+      </div>
+    )
   }
 }
